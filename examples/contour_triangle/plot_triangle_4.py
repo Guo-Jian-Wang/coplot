f0fc7e4b69c0de4a5655384c643eb85f22169c3d
Guo-Jian-Wang <gjwang@mail.bnu.edu.cn>
2020-12-28 00:24:16 +0800

    update

--- examples/contour_triangle/plot_triangle_4.py
+++ examples/contour_triangle/plot_triangle_4.py
@@ -17,8 +17,8 @@ test2 = test1 + 1
 # If you want to plot good figures, you should use it like this:
 fig = plc.Contours([test1,test2]).plot_2d(bins=150,labels=[r'$a$', r'$b$', r'$c$'],\
 colors=['r','g'],line_styles=['-','--'],fill_contours=False, show_titles=True,
-smooth=3,best_values=[[0,0,0],[1,1,1]], ticks_size=8,legend=True,\
-legend_labels=['Data 1', 'Data 2'], subplots_adjust=False)
+smooth=3,best_values=[[0,0,0],[1,1,1]],show_best_value_lines=False,ticks_size=8,
+legend=True,legend_labels=['Data 1', 'Data 2'], subplots_adjust=False)
 # and you can save the figure like this:
 # pl.savefig('test_triangle_3.pdf', fig)
 plt.show()
